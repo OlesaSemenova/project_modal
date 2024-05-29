c728598cbda34396a364847a301819485781a8d4
Olesa Semenova <semenovmaolesa@gmail.com>
2024-05-29 17:30:12 +0300

    Получение узлов dom

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const bthCloseModelWindow = document.querySelector('.close-modal-window');
+const bthShowModelWindow = document.querySelectorAll('.show-modal-window');
+
+console.log(bthShowModelWindow);
+
+for (let i = 0; i < bthShowModelWindow.length; i++) {
+  console.log(bthShowModelWindow[i]);
+}
