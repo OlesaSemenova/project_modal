756cac0d4b279f29a4d86a3eb7ff583ff93f5b38
Olesa Semenova <semenovmaolesa@gmail.com>
2024-05-29 17:54:10 +0300

    Функция открытия и закрытия модального окна

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const bthShowModelWindow = document.querySelectorAll('.show-modal-window');
 
 console.log(bthShowModelWindow);
 
+const showModalWindow = function () {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const clouseModalWindow = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < bthShowModelWindow.length; i++) {
-  console.log(bthShowModelWindow[i]);
+  bthShowModelWindow[i].addEventListener('click', showModalWindow);
 }
+
+bthCloseModelWindow.addEventListener('click', clouseModalWindow);
+overlay.addEventListener('click', clouseModalWindow);
